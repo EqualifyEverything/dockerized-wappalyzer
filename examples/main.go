27b9f01ee3a34788a85184443825adcf88b4f590
Bentley Hensel <bentley.hensel@civicactions.com>
2023-04-18 15:44:15 +0000

    Dockerized & Doc updates

---
+++ examples/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	wappalyzer "github.com/projectdiscovery/wappalyzergo"
+)
+
+func main() {
+	resp, err := http.DefaultClient.Get("https://civicactions.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, _ := ioutil.ReadAll(resp.Body) // Ignoring error for example
+
+	wappalyzerClient, err := wappalyzer.New()
+	fingerprints := wappalyzerClient.Fingerprint(resp.Header, data)
+	fmt.Printf("%v\n", fingerprints)
+
+
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/projectdiscovery/wappalyzergo
+
+go 1.16
+
+require (
+	github.com/json-iterator/go v1.1.12
+	github.com/stretchr/testify v1.8.1
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4
+)
